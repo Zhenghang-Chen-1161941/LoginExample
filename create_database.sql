224436aeeecafe9328e287c48fd7357bf35803e0
Zhenghang-Chen-1161941 <Zhenghang.Chen@lincolnuni.ac.nz>
2026-02-18 11:38:51 +1300

    login example
    login example

---
+++ create_database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE loginexample;
+
+-- Create enum type first
+CREATE TYPE user_role AS ENUM ('customer', 'staff', 'admin');
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  email VARCHAR(320) NOT NULL,
+  person_role user_role NOT NULL
+);
